5de4a099c4c1bd6599383da43d091881caaa7568
Akash <akashwarrior1@gmail.com>
2025-07-06 23:24:36 +0530

    fead: added shadcn & themes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import { nextCookies } from "better-auth/next-js";
 import prisma from "@/lib/prisma";
 
 export const auth = betterAuth({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
